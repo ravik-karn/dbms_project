5e033d75871735a7846b41b064490f2be7b95639
ravi <ravikumar.kumar102bih@gmail.com>
2015-03-01 15:19:07 +0530

    db

---
+++ project_data.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 01, 2015 at 10:45 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `project_data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE IF NOT EXISTS `contact` (
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `subject` varchar(20) DEFAULT NULL,
+  `message` varchar(140) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`name`, `email`, `subject`, `message`) VALUES
+('ravi kumar', 'ravi11305@gmail.com', 'none', ''),
+('ravi kumar karn', 'ravi11305@gmail.com', 'none', 'no message'),
+('ravi kumar', 'ravi11305@gmail.com', 'none', 'none'),
+('', '', '', ''),
+('ravi kumar karn', 'ravi11305@gmail.com', 'none', 'jaiko'),
+('', '', '', ''),
+('ravi kumar', 'ravi11305@gmail.com', 'none', 'fbcbzvf'),
+('ravi kumar', 'ravi11305@gmail.com', 'none', 'fdfdff');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
