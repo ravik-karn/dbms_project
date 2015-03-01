bae2f0a37c5738bad31e4ee5dfe60714c53fabf3
ravi <ravikumar.kumar102bih@gmail.com>
2015-03-01 14:48:44 +0530

    chaned contact.php

--- contact.php
+++ contact.php
@@ -8,5 +8,9 @@ $sub=$_POST['subject'];
 $msg=$_POST['comments'];
 $sql="INSERT INTO contact VALUES('$name','$mail','$sub','$msg');";
 mysql_query($sql,$conn);
+$subject="message";
+$message="thank you sending your response";
+mail('ravi11305@gmail.com', $subject, $message) or die("can't send");
+
 mysql_close();
 ?>
